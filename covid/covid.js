5b45fd67bb4f9d4c6b18bab64ab92d76208f124a
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-04-20 12:43:14 +0530

    add: 30 days chart

--- covid/covid.js
+++ covid/covid.js
@@ -10,6 +10,7 @@
                 $("#covid-dis").text((res.data.summary.discharged).toLocaleString());
                 $("#covid-det").text((res.data.summary.deaths).toLocaleString());
 
+                // Table
                 for(var r of res.data.regional) {
                     var o = "<tr>";
                     var loc = `<td>${r.loc}</td>`;
@@ -23,6 +24,62 @@
             } else {
                 window.alert("There has been error, please try again later or email contact@adityatd.me")
             }
+        });
+
+        // Chart
+        var his = "https://api.rootnet.in/covid19-in/stats/history";
+
+        $.get(his, function(res){
+            if(res.success) {
+                var thirty = res.data.slice(res.data.length - 29);
+
+                var days = [], total = [], active = [], deaths = [], discharged = [];
+                for(var d of thirty) {
+                    days.push(d.day);
+                    total.push(d.summary.total);
+                    active.push(d.summary.total - (d.summary.deaths + d.summary.discharged));
+                    deaths.push(d.summary.deaths);
+                    discharged.push(d.summary.discharged);
+                }
+
+                var ctx = $("#covid-cha")
+                new Chart(ctx, {
+                    type: "line",
+                    data: {
+                      labels: days,
+                      datasets: [{ 
+                          data: active,
+                          label: "Active",
+                          borderColor: "#23D160",
+                          fill: true
+                        }, { 
+                          data: total,
+                          label: "Total",
+                          borderColor: "#209CEE",
+                          fill: true
+                        }, { 
+                          data: deaths,
+                          label: "Deaths",
+                          borderColor: "#FF3860",
+                          fill: true
+                        }, { 
+                          data: discharged,
+                          label: "Discharged",
+                          borderColor: "#FFDD57",
+                          fill: true
+                        }
+                      ]
+                    },
+                    options: {
+                      response: true,
+                      maintainAspectRatio: false,
+                      title: {
+                        display: true,
+                        text: 'Last 30 Days Case History'
+                      },
+                    }
+                  });
+            } else return;
         });
     });
 }
